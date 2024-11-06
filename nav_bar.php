dfbd5e6a528f6e1bc8b49de5ec6802f655d30062
Backer <AsanRodnuan2546@gmail.com>
2024-11-06 16:40:40 +0700

    Implement user session handling in navigation bar and add logout functionality

--- nav_bar.php
+++ nav_bar.php
@@ -1,4 +1,35 @@
-<ul class="nav">
-    <li><a href="index.php">Home</a></li>
-    <li><a href="form1.php">form1</a></li>
-</ul>
+<?php
+
+    session_start();
+    include('include/include_db_oo.php');
+
+    
+    if(!isset($_SESSION['users_id'])){
+        $users_id = 0;
+    }else{
+        $users_id = $_SESSION['users_id'];
+    }
+
+
+?>
+
+
+<?php if(!$users_id){ ?>
+    <ul class="nav">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="form1.php">form1</a></li>
+    </ul>
+<?php }else{ ?>
+    <?php
+        $sql = "SELECT * FROM users WHERE users_id = '$users_id'";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+        }
+    ?>
+    <ul class="nav">
+        <li><a href="#"><?php echo $row["users_name"] ?></a></li>
+        <li><a href="logout.php">Logout</a></li>
+    </ul>
+<?php } ?>
