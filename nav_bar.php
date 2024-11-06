0a871e2d89d1777c7b16118d2c44e040082c1439
Backer <AsanRodnuan2546@gmail.com>
2024-11-06 17:14:47 +0700

    Add profile image and name to navigation bar; create profile.php and update styles

--- nav_bar.php
+++ nav_bar.php
@@ -29,7 +29,10 @@
         }
     ?>
     <ul class="nav">
-        <li><a href="#"><?php echo $row["users_name"] ?></a></li>
+        <div class="profile-container">
+                <img src="assets/images/person.png" alt="Logo" class="profile-image">
+                <a href="#"><?php echo $row["users_name"]; ?></a>
+        </div>
         <li><a href="logout.php">Logout</a></li>
     </ul>
 <?php } ?>
